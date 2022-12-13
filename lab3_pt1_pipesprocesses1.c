0356cee217598d05b2b84c6ff0c15b6034a355d5
Chad Toomer <70294921+chadtoomer@users.noreply.github.com>
2022-12-13 18:43:07 -0500

    Create lab3_pt1_pipesprocesses1.c

---
+++ lab3_pt1_pipesprocesses1.c
@@ -0,0 +1,89 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<string.h> 
+#include<sys/wait.h> 
+
+//Credit to Professor Burge Recorded Letures and Deante Taylor
+  
+int main() 
+{ 
+   
+  
+    int fd1[2];  // Used to store two ends of first pipe 
+    int fd2[2];  // Used to store two ends of second pipe 
+  
+    char input_str[100]; 
+    char sec_input_str[100]; 
+    pid_t p; 
+  
+    if (pipe(fd1)==-1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+    if (pipe(fd2)==-1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+  
+    printf("Enter a string to concatenate:");
+    scanf("%s", input_str);
+    input_str[101] ='\0'; //input null terminator
+    p = fork();
+  
+    if (p < 0) 
+    {
+      fprintf(stderr, "fork Failed" ); 
+      return 1; 
+    } 
+  
+    // Parent process 
+  
+    else if (p > 0) 
+    { 
+      close(fd1[0]);  // Close reading end of pipes 
+  
+      
+      write(fd1[1], input_str, strlen(input_str)+1);
+      read(fd2[0], input_str, 100);
+        
+     
+      wait(NULL);
+      
+      printf("Parent concatenated string: %s\n", input_str);
+  
+      close(fd2[1]); // Close writing end of pipes 
+      close(fd1[1]); 
+    } 
+  
+    // Child process
+    
+  else
+  {
+    close(fd1[1]);  // Close writing end of first pipes 
+    close(fd2[0]);  // Close the reading end of 2nd pipe
+      
+   
+    read(fd1[0], input_str, 100); 
+  
+    strcat(input_str, "howard.edu");
+  
+    printf("Child concatenated string: %s\n", input_str);
+    
+    printf("Enter a string to concatenate:");
+    scanf("%s", sec_input_str);
+
+    strcat(input_str, sec_input_str);
+
+    write(fd2[1], input_str, strlen(input_str)+1);
+    
+    // Close both reading ends 
+    close(fd1[0]); 
+    close(fd2[1]); 
+    
+    exit(0); 
+  } 
+}
